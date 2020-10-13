0cf9623973c8ab34c10c563e390981c29ebbe4ac
José Martins <j.msantos.netto@gmail.com>
2020-10-12 21:59:34 -0300

    Initial Commit

---
+++ MyMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMoviesApp
+//
+//  Created by Jose Martins on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
